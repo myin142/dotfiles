b69df7d727e9cc288bb35024abba6e9e882f41cf
Min Yin <minyin142p@gmail.com>
2018-05-14 18:22:03 +0200

    check for internet connection

--- install/installPkg.sh
+++ install/installPkg.sh
@@ -1,31 +1,37 @@
 #!/bin/sh
 
-# Install Core Packages for
-sudo pacman -S - < core
+runProgram(){
+	# Install Core Packages for
+	sudo pacman -S - < core
 
-sudo systemctl enable ntpd
-sudo systemctl enable lightdm
+	sudo systemctl enable ntpd
+	sudo systemctl enable lightdm
 
-# Fix for Black Screen on SSD, has to be executed as root
-su root -c 'printf "[LightDM]\nlogind-check-graphical=true\nrun-directory=/run/lightdm\n\n[Seat:*]\nsession-wrapper=/etc/lightdm/Xsession" > /etc/lightdm/lightdm.conf'
+	# Fix for Black Screen on SSD, has to be executed as root
+	su root -c 'printf "[LightDM]\nlogind-check-graphical=true\nrun-directory=/run/lightdm\n\n[Seat:*]\nsession-wrapper=/etc/lightdm/Xsession" > /etc/lightdm/lightdm.conf'
 
-# Install Yaourt
-curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
-tar -xvzf package-query.tar.gz
-cd package-query
-makepkg -si
+	# Install Yaourt
+	curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
+	tar -xvzf package-query.tar.gz
+	cd package-query
+	makepkg -si
 
-cd ..
-curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
-tar -xvzf yaourt.tar.gz
-cd yaourt
-makepkg -si
+	cd ..
+	curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+	tar -xvzf yaourt.tar.gz
+	cd yaourt
+	makepkg -si
 
-cd ..
-rm package-query -r
-rm yaourt -r
-rm package-query.tar.gz
-rm yaourt.tar.gz
+	cd ..
+	rm package-query -r
+	rm yaourt -r
+	rm package-query.tar.gz
+	rm yaourt.tar.gz
 
-# Install Additional Packages
-sudo pacman -S - < extra
+	# Install Additional Packages
+	sudo pacman -S - < extra
+}
+
+# Check if internet connection if available
+ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && runProgram \
+	|| echo "No Internet Connection" && exit 1
