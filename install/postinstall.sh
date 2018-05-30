7c12d57c4127daa44e3bcc5992159ef86192a38f
Min Yin <minyin142p@gmail.com>
2018-05-30 22:19:52 +0200

    change bin folder to ~/.bin

--- install/postinstall.sh
+++ install/postinstall.sh
@@ -14,17 +14,17 @@ ln -sf ~/.dotfiles/config/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/config/xinitrc ~/.xinitrc
 
 if [ -d ~/.config/i3 ]; then
-	rm ~/.config/i3 -r
+	rm ~/.config/i3
 fi
 ln -sf ~/.dotfiles/config/i3 ~/.config/i3
 
 if [ -d ~/.config/kitty ]; then
-	rm ~/.config/kitty -r
+	rm ~/.config/kitty
 fi
 ln -sf ~/.dotfiles/config/kitty ~/.config/kitty
 
 if [ -d ~/.fonts ]; then
-	rm ~/.fonts -r
+	rm ~/.fonts
 fi
 ln -sf ~/.dotfiles/fonts ~/.fonts
 
@@ -32,6 +32,11 @@ ln -sf ~/.dotfiles/img/wallpaper.png ~/.wallpaper.png
 ln -sf ~/.dotfiles/img/lock.png ~/.lock.png
 
 if [ -d ~/.vim ]; then
-	rm ~/.vim -r
+	rm ~/.vim
 fi
 ln -sf ~/.dotfiles/vim ~/.vim
+
+if [ -d ~/.bin ]; then
+	rm ~/.bin
+fi
+ln -sf ~/.dotfiles/bin ~/.bin
