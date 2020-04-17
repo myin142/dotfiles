b795bf3b228ed18e5bb69077d18bde2386722e9e
myin <minyin142p@gmail.com>
2020-04-17 15:38:35 +0200

    Update nvim setup

--- install/nvim/setup.sh
+++ install/nvim/setup.sh
@@ -2,6 +2,10 @@
 
 sudo pacman -S neovim
 
-cp nvim/ ~/.config/nvim -r
+if [ -d ~/.config/nvim ]; then
+    rm ~/.config/nvim -rf
+fi
+
+ln -sf "$(pwd)/nvim/" ~/.config/nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
