5b7096a37f132d88bc76335f9fcfe12f156d6d6c
myin <minyin142p@gmail.com>
2024-10-28 22:18:08 +0100

    fix

--- general/bin/combine-sprites.sh
+++ general/bin/combine-sprites.sh
@@ -4,4 +4,4 @@ INPUT=$1
 OUTPUT=$2
 COLUMNS=${3:-10}
 
-python ~/Documents/scripts/anim_combine.py -d $INPUT -o $OUTPUT-c $COLUMN
+python ~/Documents/scripts/anim_combine.py -d $INPUT -o $OUTPUT -c $COLUMN
