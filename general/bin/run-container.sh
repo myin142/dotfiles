d53e88b630217321e36c0095afa03115cb51893f
myin <minyin142p@gmail.com>
2025-09-16 21:43:28 +0200

    final patches for steam containers

--- general/bin/run-container.sh
+++ general/bin/run-container.sh
@@ -10,11 +10,10 @@ if [ -z $CONTAINER_NAME ]; then
 fi
 
 if [ ! -d "$CONTAINER_NAME" ]; then
-    # btrfs subvolume create $CONTAINER_NAME
-
-    CODENAME=${2:-jammy}
-    REPO="https://mirror.easyname.at/ubuntu-archive/"
-    sudo debootstrap --include=systemd-container --components=main,universe,multiverse $CODENAME $CONTAINER_NAME $REPO
+    mkdir arch
+    sudo pacstrap -K -c $CONTAINER_NAME base steam vulkan-icd-loader vulkan-intel vulkan-radeon pulseaudio
+    sudo systemd-nspawn -D $CONTAINER_NAME --bind=$HOME/.bin:/home/bin /home/bin/setup-steam-container
+    sudo systemd-nspawn -D $CONTAINER_NAME passwd
 fi
 
 XAUTH="/tmp/${CONTAINER_NAME}_xauth"
@@ -22,11 +21,19 @@ xauth nextract - "$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f "$XAUTH" nmerge 
 
 # xhost +local:
 
-# --bind=$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/wayland.sock --setenv=WAYLAND_DISPLAY=/tmp/wayland.sock \
+HOST_PULSE=/run/user/$UID/pulse
+CONTAINER_PULSE=/run/user/host/pulse
+
+# HOST_BUS=/run/user/$UID/bus
+# CONTAINER_BUS=/run/user/host/bus
+# --bind-ro=$HOST_BUS:$CONTAINER_BUS --setenv=DBUS_SESSION_BUS_ADDRESS=unix:path=$CONTAINER_BUS \
+
 sudo systemd-nspawn -D $CONTAINER_NAME \
     --user=steam \
     --bind-ro=/etc/resolv.conf \
     --bind=/tmp/.X11-unix --setenv=DISPLAY=$DISPLAY \
-    --bind="$XAUTH" --setenv=XAUTHORITY="$XAUTH" \
     --bind=/dev/dri --property=DeviceAllow='char-drm rw' \
-    --bind=/home/myin/Documents/games:/home/games
+    --bind="$XAUTH" --setenv=XAUTHORITY="$XAUTH" \
+    --bind=$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/wayland.sock --setenv=WAYLAND_DISPLAY=/tmp/wayland.sock \
+    --bind-ro=$HOST_PULSE:$CONTAINER_PULSE --setenv=PULSE_SERVER=unix:$CONTAINER_PULSE/native \
+    --bind=$HOME/Documents/games:/home/games
